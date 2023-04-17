332097decdfc71126a9e3a0db3a88e13d2963c1a
seongjun <psjj7604@gmail.com>
2023-04-18 03:22:34 +0900

    도메인 구현

--- src/main/java/com/squarecross/photoalbum/domain/Album.java
+++ src/main/java/com/squarecross/photoalbum/domain/Album.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "album", schema = "photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "album_id")})
@@ -20,6 +21,10 @@ public class Album {
     @Column(name = "created_at", unique = false, nullable = true)
     @CreationTimestamp
     private Date createdAt;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "album", cascade = CascadeType.ALL)
+    private List<Photo> photos;
+
 
 
     public Album() {}
@@ -46,6 +51,14 @@ public class Album {
 
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
     }
 }
 
