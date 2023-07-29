82f5e2ef665ee62d99ef7ea1086bd9261ee9635a
seongjun <psjj7604@gmail.com>
2023-07-29 12:41:02 +0900

    업로드 기준 최신 4개를 반환

--- src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
@@ -3,7 +3,13 @@ package com.squarecross.photoalbum.repository;
 import com.squarecross.photoalbum.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
     int countByAlbum_AlbumId(Long AlbumId);
     int countByAlbum_AlbumName(String AlbumName);
+
+    List<Photo> findTop4ByAlbum_AlbumIdOrderByUploadedAtDesc(Long AlbumId);
+
+
 }
