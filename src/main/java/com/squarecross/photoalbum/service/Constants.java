8a81eba276602d89c85c41964ea9a212f2d91747
seongjun <psjj7604@gmail.com>
2023-08-01 14:54:45 +0900

    썸네일 사이즈 300으로 고정

--- src/main/java/com/squarecross/photoalbum/service/Constants.java
+++ src/main/java/com/squarecross/photoalbum/service/Constants.java
@@ -2,4 +2,5 @@ package com.squarecross.photoalbum.service;
 
 public class Constants {
     public static final String PATH_PREFIX = "/Users/seongjun/IdeaProjects/photoalbum";
+    public static final int THUMB_SIZE = 300;
 }
